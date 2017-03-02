823454cbe41697eb215e34855c2b9cb68a96d60c
Leo Paulisic <leopaulisic@gmail.com>
2017-03-02 19:55:53 +0100

    Photo Capture and Upload
    Error,thumbail

--- FireBaseDemo/app/src/main/java/com/example/leo/firebasedemo/MainActivity.java
+++ FireBaseDemo/app/src/main/java/com/example/leo/firebasedemo/MainActivity.java
@@ -3,23 +3,27 @@ package com.example.leo.firebasedemo;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mSelectImage;
+    private Button mUpload;
+    private ImageView mImageView;
     private StorageReference mStorage;
-    private static final int GALLERY_INTENT=2;
-    private ProgressDialog mProgressDialog;
+    private ProgressDialog mProgress;
+    private static final int CAMERA_REQUEST_CODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +31,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mStorage = FirebaseStorage.getInstance().getReference();
+        mUpload = (Button) findViewById(R.id.upload);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        mProgress = new ProgressDialog(this);
 
-        mSelectImage = (Button) findViewById(R.id.button);
-        mProgressDialog = new ProgressDialog(this);
-
-        mSelectImage.setOnClickListener(new View.OnClickListener() {
+        mUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent,GALLERY_INTENT);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,CAMERA_REQUEST_CODE);
             }
         });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+        if(requestCode==CAMERA_REQUEST_CODE && resultCode==RESULT_OK){
 
-            mProgressDialog.setMessage("Uploading ...");
-            mProgressDialog.show();
+            mProgress.setMessage("Uploading image ...");
+            mProgress.show();
             Uri uri = data.getData();
             StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
             filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(MainActivity.this,"Upload done",Toast.LENGTH_SHORT).show();
-                    mProgressDialog.dismiss();
+
+                    Uri downladUri = taskSnapshot.getDownloadUrl();
+                    Picasso.with(MainActivity.this).load(downladUri).fit().centerCrop().into(mImageView);
+                    Toast.makeText(MainActivity.this,"Upload finished ...",Toast.LENGTH_SHORT).show();
+                    mProgress.dismiss();
+
                 }
             });
         }
